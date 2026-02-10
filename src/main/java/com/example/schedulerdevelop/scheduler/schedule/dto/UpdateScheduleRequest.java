e94666959f4ead5f61d0430f875955c1201772a0
Taeguni06 <aqwsde0412@gmail.com>
2026-02-10 20:58:08 +0900

    feat. validation 추가

--- src/main/java/com/example/schedulerdevelop/scheduler/schedule/dto/UpdateScheduleRequest.java
+++ src/main/java/com/example/schedulerdevelop/scheduler/schedule/dto/UpdateScheduleRequest.java
@@ -1,8 +1,12 @@
 package com.example.schedulerdevelop.scheduler.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateScheduleRequest {
+    @NotBlank
+    @Size(max = 20, message = "일정 제목은 20자 이내입니다.")
     private String title;
 }
--- src/main/java/com/example/schedulerdevelop/scheduler/user/dto/UpdateUserRequest.java
+++ src/main/java/com/example/schedulerdevelop/scheduler/user/dto/UpdateUserRequest.java
@@ -1,10 +1,17 @@
 package com.example.schedulerdevelop.scheduler.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateUserRequest {
+    @Size(max = 10, message = "이름은 10자 이하입니다.")
+    @NotBlank
     private String name;
+    @Email
+    @NotBlank
     private String email;
     private String password;
 }
